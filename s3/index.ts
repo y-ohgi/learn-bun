de7385e7b61d73a4d64683552290504edcf23e32
y-ohgi <ohgi.211@gmail.com>
2025-01-24 15:55:35 +0900

    add: bun s3 api example

--- s3/index.ts
+++ s3/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import { env, s3 } from "bun";
+
+const BUCKET = env.BUCKET;
+
+const file = s3.file(`s3://${BUCKET}/test.json`);
+
+console.log(await file.json());
+
+// MEMO: アップロード用の署名付きURLを取得する
+// const upload = s3.presign(`s3://${BUCKET}/upload.json`, {
+//   method: "PUT",
+//   type: "application/json",
+// });
+
+// console.log(upload)
+
+// MEMO: 署名付きURLを取得する
+// const download = s3.presign(`s3://${BUCKET}/test.json`);
+
+// console.log(download);
